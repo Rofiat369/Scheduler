086b0a1c987943c2085e452d10fbad4088cd9652
Rofiat <rofiatolusanya12@gmail.com>
2022-01-18 22:06:50 -0500

    created styles for button using storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  let buttonClass = "button";
+
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  }
+
+  if (props.danger) {
+    buttonClass += " button--danger";
+  }
+
+  return (
+    <Fragment>
+      <button
+        className={buttonClass}
+        onClick={props.onClick}
+        disabled={props.disabled}
+      >
+        {props.children}
+      </button>
+    </Fragment>
+  );
 }
