307c66ad7cf770d80ce9ba0b03e6b2295d86db38
Rofiat <rofiatolusanya12@gmail.com>
2022-01-21 13:55:19 -0500

    Added a component to display form for name entry

---
+++ src/components/Appointment/form.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+import { useState } from "react";
+
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent("")
+    setInterviewer(null)
+  }
+
+  function cancel () {
+    reset();
+    props.onCancel()
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.onSave(student, interviewer)
+  }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={(event)=>handleSubmit(event)}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name={props.name}
+            value ={student}
+            type="text"
+            onChange={(event) => setStudent(event.target.value)}  
+            placeholder="Enter Student Name"
+            /*
+          This must be a controlled component
+          your code goes here
+        */
+          />
+        </form>
+        <InterviewerList interviewers={props.interviewers} onChange={setInterviewer} value={interviewer} />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={(event)=>handleSubmit(event)} >
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
+}
