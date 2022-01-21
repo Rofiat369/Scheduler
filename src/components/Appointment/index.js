e4907d2c1cbd0d01291cd113fad5f82a00e412dd
Rofiat <rofiatolusanya12@gmail.com>
2022-01-21 13:56:59 -0500

    added a file to compile all the files in appointment folder

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,2 +1,18 @@
-import Appointment from "components/Appointment";
+import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "./header";
+import Show from "./show";
+import Empty from "./empty";
+
+
+export default function Appointments(props){
+  const showOrEmpty = props.interview? <Show /> : <Empty />;
+
+return(
+<article className="appointment">
+<Header time={props.time}/>
+{showOrEmpty}
+
+</article>
+);
+}
