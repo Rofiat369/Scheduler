06484dbed232c5d5b59aa38d156f7ac63edb02ab
Rofiat <rofiatolusanya12@gmail.com>
2022-01-21 22:22:53 -0500

    Created a selector

---
+++ src/helpers/selectors.js
@@ -0,0 +1,52 @@
+// export function getAppointmentsForDay(state, day) {
+  
+//   if (!state.days) {
+//     return [];
+//   }
+//   let theDay = state.days.find(item => item.name === day);
+  
+//   if (theDay === undefined) {
+//     return [];
+//   }
+//   let result = [];
+//   for (const id of theDay.appointments) {
+//     const appointmentObj = state.appointments[id];
+//     result.push(appointmentObj);
+//   }
+
+//   return result;
+// }
+
+export function getAppointmentsForDay(state, day) {
+  console.log( "This is the state", state)
+  const { days, appointments } = state;
+  const filteredDay = days.find(item => day === item.name);
+  if (days.length < 1 || filteredDay === undefined) {
+    return [];
+  }
+  const daysAppointment = filteredDay.appointments.map(id => appointments[id]);
+  return daysAppointment;
+}
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+  const interviewObject = {
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer]
+  };
+  return interviewObject;
+}
+
+export function getInterviewersForDay(state, day) {
+  const { days, interviewers } = state;
+  const filteredDay = days.find(item => day === item.name);
+  if (days.length < 1 || filteredDay === undefined) {
+    return [];
+  }
+  const daysInterviewers = filteredDay.interviewers.map(
+    interview => interviewers[interview]
+  );
+  return daysInterviewers;
+}
