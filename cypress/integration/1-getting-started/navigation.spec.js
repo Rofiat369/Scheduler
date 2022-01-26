6c32d74a43a1a5f627314fa373bbb5a9f17fe5ec
Rofiat <rofiatolusanya12@gmail.com>
2022-01-26 08:53:03 -0500

    Added cypress to project for end-2-end testing

---
+++ cypress/integration/1-getting-started/navigation.spec.js
@@ -0,0 +1,11 @@
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("Should navigate to Tuesday", () => {
+    cy.contains("li", "Tuesday")
+      .click()
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
+  });
+});
